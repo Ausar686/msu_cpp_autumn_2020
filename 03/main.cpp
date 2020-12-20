4861f5bfb205b1dc8855c893b48b2ed01931c700
GlebYushkov <glebyushkov@mail.ru>
2020-12-21 00:07:17 +0300

    hw3 added

--- 03/main.cpp
+++ 03/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cassert>
+#include <sstream>
 #include "matrix.h"
 
 using namespace std;
@@ -73,6 +74,24 @@ wrong_size_test(void)
     }
 }
 
+void
+print_test(void)
+{
+    std::stringstream stream1, stream2;
+    std::string str1, str2;
+    Matrix m = Matrix(2, 2);
+    m[0][0] = 0;
+    m[0][1] = 1;
+    m[1][0] = 2;
+    m[1][1] = 3;
+    stream1 << m;
+    stream1 >> str1;
+    stream2 << 0 << ' ' << 1 << ' ' << std::endl << 2 << ' ' << 3 << ' ' << std::endl;
+    stream2 >> str2;
+    assert(str1 == str2);
+    std::cout << "PRINT_TEST COMPLETE" << std::endl;
+}
+
 int
 main(int argc, char**argv)
 {
@@ -82,6 +101,7 @@ main(int argc, char**argv)
     mul_correct_test();
     equality_test();
     wrong_size_test();
+    print_test();
     std::cout << "TESTING COMPLETED" << std::endl;
     return 0;
 }
